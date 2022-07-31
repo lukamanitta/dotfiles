a6dc4830fdc8d356149a60b9b246e6a7279d77ca
lukamanitta <lukablueowl51@gmail.com>
2022-07-31 22:13:36 +1000

    feat: properly highlighting window separators

--- nvim/.config/nvim/lua/plugins/colourschemes/init.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/init.lua
@@ -1,8 +1,14 @@
+local cmd = vim.cmd
+
+local get_hi_group_bg = require("utils.config").get_hi_group_bg
 local colourscheme = require("settings.globals").colourscheme
 
 local require_path = colourscheme:gsub("_", ".")
 
 require("plugins.colourschemes." .. require_path)
 
+if vim.o.laststatus == 3 then
+    cmd("hi WinSeparator guibg=None guifg=" .. get_hi_group_bg("StatusLine"))
+end
 require("plugins.lualine")
 require("plugins.cokeline")
