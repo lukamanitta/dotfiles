2ca21ff3ccf55ba54f8ac6350a94a6f402b776c1
lukaaa-m <luka@lukamanitta.com>
2022-08-02 10:24:34 +1000

    fix(wip): debugging treesitter errors in markdown files

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
     -- ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    disable = { "markdown" },
+    disable = { "markdown", "pseudo", "dafny" },
 
     highlight = {
         enable = true,
