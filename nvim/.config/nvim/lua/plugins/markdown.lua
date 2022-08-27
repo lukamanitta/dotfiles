99443b67d6915f1086e3928d1ca14ef6d973badb
lukamanitta <lukablueowl51@gmail.com>
2022-08-27 16:11:02 +1000

    feat: change markdown options

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -10,10 +10,8 @@ apply_globals({
     vim_markdown_frontmatter = 1,
     vim_markdown_strikethrough = 1,
     vim_markdown_no_extensions_in_markdown = 0,
-    vim_markdown_autowrite = 1,
+    vim_markdown_autowrite = 0,
     vim_markdown_new_list_item_indent = 0,
     vim_markdown_auto_insert_bullets = 0,
     vim_markdown_edit_url_in = "current",
 })
-
-vim.cmd("silent map <CR> <Plug>Markdown_EditUrlUnderCursor")
