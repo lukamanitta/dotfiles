b62fbd1dfac3429f20d3a8091fa53320faa5b199
lukaaa-m <luka@lukamanitta.com>
2022-10-11 13:30:29 +1000

    refactor: add back virtual text for diagnostics + reformat

--- nvim/.config/nvim/lua/lsp/cosmetics.lua
+++ nvim/.config/nvim/lua/lsp/cosmetics.lua
@@ -4,18 +4,30 @@ local lsp_icons = require("assets.icons").lsp
 vim.diagnostic.config({
     signs = true,
     underline = true,
-    update_in_insert = false,
-    virtual_text = false,
-    -- virtual_text = {
-    --     prefix = '●', -- Could be '●', '▎', 'x', '■'
-    -- }
+    update_in_insert = true,
+    -- virtual_text = false,
+    virtual_text = {
+        prefix = "●", -- Could be '●', '▎', 'x', '■'
+    },
 })
 
 fn.sign_define("DiagnosticSignError", {
     text = lsp_icons.Error .. " ",
     texthl = "DiagnosticError",
 })
-fn.sign_define("DiagnosticSignWarn", { text = lsp_icons.Warn .. " ", texthl = "DiagnosticWarn" })
-fn.sign_define("DiagnosticSignHint", { text = lsp_icons.Hint .. " ", texthl = "DiagnosticHint" })
-fn.sign_define("DiagnosticSignInfo", { text = lsp_icons.Info .. " ", texthl = "DiagnosticInfo" })
-fn.sign_define("LightBulbSign", { text = lsp_icons.CodeAction .. " ", texthl = "DiagnosticWarn" })
+fn.sign_define(
+    "DiagnosticSignWarn",
+    { text = lsp_icons.Warn .. " ", texthl = "DiagnosticWarn" }
+)
+fn.sign_define(
+    "DiagnosticSignHint",
+    { text = lsp_icons.Hint .. " ", texthl = "DiagnosticHint" }
+)
+fn.sign_define(
+    "DiagnosticSignInfo",
+    { text = lsp_icons.Info .. " ", texthl = "DiagnosticInfo" }
+)
+fn.sign_define(
+    "LightBulbSign",
+    { text = lsp_icons.CodeAction .. " ", texthl = "DiagnosticWarn" }
+)
