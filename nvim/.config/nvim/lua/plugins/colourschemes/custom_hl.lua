d499deb26e42c2441146020206ea6825919fd879
lukamanitta <lukablueowl51@gmail.com>
2022-10-14 22:27:34 +1000

    refactor: change highlight group from TSFunctionCall to @function.call

--- nvim/.config/nvim/lua/plugins/colourschemes/custom_hl.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/custom_hl.lua
@@ -2,4 +2,4 @@ local cmd = vim.cmd
 local get_hi_group_fg = require("utils.color.hl_groups").get_hi_group_fg
 local get_hi_group_bg = require("utils.color.hl_groups").get_hi_group_bg
 
-cmd("hi! TSFunctionCall gui=bold guifg=" .. get_hi_group_fg("Function"))
+cmd("hi! @function.call gui=bold guifg=" .. get_hi_group_fg("Function"))
