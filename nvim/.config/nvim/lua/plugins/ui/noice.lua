d01affd61ea62d03f8cebee09181acc08e2132bb
lukaaa-m <luka@lukamanitta.com>
2022-10-24 22:54:10 +1000

    feat: updated noice configuration for new api

--- nvim/.config/nvim/lua/plugins/ui/noice.lua
+++ nvim/.config/nvim/lua/plugins/ui/noice.lua
@@ -1,54 +1,72 @@
 ---@diagnostic disable: undefined-doc-name
 local general_icons = require("assets.icons").general
+local comp_icons = require("assets.icons").types
 local cmdline_hl_group = "Conditional"
 
 require("noice").setup({
     cmdline = {
         enabled = true, -- disable if you use native command line UI
         view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-        opts = { buf_options = { filetype = "vim" } }, -- enable syntax highlighting in the cmdline
-        icons = {
-            ["/"] = {
-                icon = " " .. general_icons.Search .. " ",
-                hl_group = "DiagnosticWarn",
-            },
-            ["?"] = {
-                icon = " " .. general_icons.Search .. " ",
-                hl_group = "DiagnosticWarn",
-            },
-            [":"] = {
+        opts = { lang = "vim" }, -- enable syntax highlighting in the cmdline
+        format = {
+            cmdline = {
+                pattern = "^:",
                 icon = " " .. general_icons.Shell .. " ",
-                hl_group = cmdline_hl_group,
-                firstc = false,
             },
+            search_down = {
+                kind = "search",
+                pattern = "^/",
+                icon = " " .. general_icons.Search .. " ",
+            },
+            search_up = {
+                kind = "search",
+                pattern = "^%?",
+                icon = " " .. general_icons.Search .. " ",
+            },
+            filter = { pattern = "^:%s*!", icon = " $ ", lang = "sh" },
+            lua = { pattern = "^:%s*lua%s+", icon = "  ", lang = "lua" },
         },
     },
     messages = {
-        -- NOTE: If you enable noice messages UI, noice cmdline UI is enabled
-        -- automatically. You cannot enable noice messages UI only.
-        -- It is current neovim implementation limitation.  It may be fixed later.
-        enabled = false, -- disable if you use native messages UI
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = false, -- enables the Noice messages UI
+        view = "notify", -- default view for messages
+        view_error = "notify", -- view for errors
+        view_warn = "notify", -- view for warnings
+        view_history = "split", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
     },
     popupmenu = { -- cmdline completion
         enabled = true, -- disable if you use something like cmp-cmdline
         ---@type 'nui'|'cmp'
         backend = "nui", -- backend to use to show regular cmdline completions
         -- You can specify options for nui under `config.views.popupmenu`
+        kind_icons = comp_icons, -- set to `false` to disable icons
     },
     history = {
         -- options for the message history that you get with `:Noice`
         view = "split",
-        opts = { enter = true },
+        opts = { enter = true, format = "details" },
         filter = {
-            event = "msg_show",
+            event = { "msg_show", "notify" },
             ["not"] = { kind = { "search_count", "echo" } },
         },
     },
     notify = {
         enabled = false,
+        view = "notify",
     },
-    hacks = {
-        skip_duplicate_messages = true,
+    lsp_progress = {
+        enabled = false,
+        -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+        -- See the section on formatting for more details on how to customize.
+        --- @type NoiceFormat|string
+        format = "lsp_progress",
+        --- @type NoiceFormat|string
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30, -- frequency to update lsp progress message
+        view = "mini",
     },
     throttle = 1000 / 30,
 
