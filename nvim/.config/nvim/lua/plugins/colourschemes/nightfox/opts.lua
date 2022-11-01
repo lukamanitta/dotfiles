adcfcceea941550e9474369f4b2c058aa0fe9fe0
lukamanitta <lukablueowl51@gmail.com>
2022-11-02 07:19:50 +1000

    refactor: move tokyonight colourscheme opts to separate file

--- nvim/.config/nvim/lua/plugins/colourschemes/nightfox/opts.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/nightfox/opts.lua
@@ -1,5 +1,4 @@
 local opts = {
-
     -- Compiled file's destination location
     compile_path = vim.fn.stdpath("cache") .. "/nightfox",
     compile_file_suffix = "_compiled", -- Compiled file suffix
--- nvim/.config/nvim/lua/plugins/colourschemes/tokyonight/storm.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/tokyonight/storm.lua
@@ -1,21 +1,10 @@
 local apply_globals = require("utils.helpers").apply_globals
 local cmd = vim.cmd
+local opts = require("plugins.colourschemes.tokyonight.opts")
 
 apply_globals({
     tokyonight_style = "storm", -- storm | night | day
-    tokyonight_terminal_colors = true,
-    tokyonight_italic_comments = true,
-    tokyonight_italic_keywords = true,
-    tokyonight_italic_functions = false,
-    tokyonight_italic_variables = false,
-    tokyonight_transparent = false,
-    tokyonight_hide_inactive_statusline = false,
-    tokyonight_sidebars = { "NvimTree", "Trouble" },
-    tokyonight_transparent_sidebar = false,
-    tokyonight_dark_sidebar = true,
-    tokyonight_dark_float = true,
-    tokyonight_day_brightness = 1,
-    tokyonight_lualine_bold = false,
+    opts,
 })
 
 cmd("set background=dark")
