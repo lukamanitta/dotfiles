f9cba26dae7c1509aee408e5ca99aadc9f5aa18a
lukamanitta <lukablueowl51@gmail.com>
2022-11-09 21:52:26 +1000

    feat: adjusted gruvbox highlights + added wezterm scheme

--- nvim/.config/nvim/lua/plugins/colourschemes/gruvbox/dark.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/gruvbox/dark.lua
@@ -1,9 +1,16 @@
 local opts = require("plugins.colourschemes.gruvbox.opts")
+local palette = require("gruvbox.palette")
+local table_merge = require("utils.tables").table_merge
 
-require("gruvbox").setup(vim.tbl_deep_extend("force", opts, {
-    contrast = "hard",
-}))
-
-vim.cmd("hi! link SignColumn Normal")
 vim.cmd("set background=dark")
+
+require("gruvbox").setup({
+    inverse = false,
+    contrast = "hard",
+})
+
 vim.cmd("colorscheme gruvbox")
+
+require("plugins.colourschemes.gruvbox.custom_hl")
+
+vim.cmd("hi! StatusLine guibg=#3c3836 guifg=#d5c4a1 gui=none cterm=none")
--- nvim/.config/nvim/lua/plugins/colourschemes/gruvbox/light.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/gruvbox/light.lua
@@ -1,8 +1,14 @@
 local opts = require("plugins.colourschemes.gruvbox.opts")
 
-require("gruvbox").setup(vim.tbl_deep_extend("force", opts, {
-    contrast = "soft",
-}))
-
 vim.cmd("set background=light")
+
+require("gruvbox").setup({
+    inverse = false,
+    contrast = "hard",
+})
+
 vim.cmd("colorscheme gruvbox")
+
+require("plugins.colourschemes.gruvbox.custom_hl")
+
+vim.cmd("hi! StatusLine guibg=#ebdbb2 guifg=#504945 gui=none cterm=none")
