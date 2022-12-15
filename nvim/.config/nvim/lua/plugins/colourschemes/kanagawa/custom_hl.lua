df29e3d43c14aca44fdca3dae498a84bf8350a16
lukaaa-m <luka@lukamanitta.com>
2022-12-16 09:31:18 +1000

    feat: add rainbow cols to kanagawa & change colourscheme to kanagawa

---
+++ nvim/.config/nvim/lua/plugins/colourschemes/kanagawa/custom_hl.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_set_hl(0, "rainbowcol1", {
+    fg = "#C34043",
+})
+vim.api.nvim_set_hl(0, "rainbowcol2", {
+    fg = "#FF9E3B",
+})
+vim.api.nvim_set_hl(0, "rainbowcol3", {
+    fg = "#98BB6C",
+})
+vim.api.nvim_set_hl(0, "rainbowcol4", {
+    fg = "#6A9589",
+})
+vim.api.nvim_set_hl(0, "rainbowcol5", {
+    fg = "#7E9CD8",
+})
+vim.api.nvim_set_hl(0, "rainbowcol6", {
+    fg = "#D27E99",
+})
+vim.api.nvim_set_hl(0, "rainbowcol7", {
+    fg = "#957FB8",
+})
--- nvim/.config/nvim/lua/plugins/colourschemes/kanagawa/dark.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/kanagawa/dark.lua
@@ -2,14 +2,16 @@ local cmd = vim.cmd
 local opts = require("plugins.colourschemes.kanagawa.opts")
 
 require("kanagawa").setup(vim.tbl_deep_extend("force", opts, {
-    colors = {
-        bg_light0 = "#16161d",
-    },
+    -- colors = {
+    --     bg_light0 = "#16161d",
+    -- },
     overrides = {},
     theme = "default",
 }))
 
 cmd("set background=dark")
 cmd("colorscheme kanagawa")
+
+require("plugins.colourschemes.kanagawa.custom_hl")
 
 cmd("hi link NvimTreeNormalNC NvimTreeNormal")
