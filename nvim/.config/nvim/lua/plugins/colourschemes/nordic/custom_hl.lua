4623b01b752f52beacf299aa9052e4e298b66785
lukaaa-m <luka@lukamanitta.com>
2023-01-27 14:38:33 +1000

    feat: add dimmed background to diagnostic virtual text for nordic

--- nvim/.config/nvim/lua/plugins/colourschemes/nordic/custom_hl.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/nordic/custom_hl.lua
@@ -21,3 +21,23 @@ vim.api.nvim_set_hl(0, "rainbowcol6", {
 vim.api.nvim_set_hl(0, "rainbowcol7", {
     fg = nord_palette.magenta.base,
 })
+
+local get_hi_group_fg = require("utils.color.hl_groups").get_hi_group_fg
+local get_hi_group_bg = require("utils.color.hl_groups").get_hi_group_bg
+local blend_rgb = require("utils.color").blend_rgb
+
+local diagnostic_types = { "Error", "Warn", "Hint", "Info" }
+for _, diagnostic_type in ipairs(diagnostic_types) do
+    vim.cmd(
+        "hi DiagnosticVirtualText"
+        .. diagnostic_type
+        .. " guifg="
+        .. get_hi_group_fg("DiagnosticVirtualText" .. diagnostic_type)
+        .. " guibg="
+        .. blend_rgb(
+            get_hi_group_bg("Normal"),
+            get_hi_group_fg("DiagnosticVirtualText" .. diagnostic_type),
+            0.1
+        )
+    )
+end
