a6fd0013b554241bd78caa1fed6a140b80b7527a
lukaaa-m <luka@lukamanitta.com>
2023-01-30 22:05:01 +1000

    feat: add signcolumn transparency for transparency mode

--- nvim/.config/nvim/lua/plugins/colourschemes/init.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes/init.lua
@@ -27,4 +27,6 @@ vim.cmd(
 if require("settings.globals").transparent_bg then
     vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
     vim.cmd("hi NormalNC ctermbg=NONE guibg=NONE")
+    vim.cmd("hi SignColumn ctermbg=NONE guibg=NONE")
+    vim.cmd("hi SignColumnSB ctermbg=NONE guibg=NONE")
 end
