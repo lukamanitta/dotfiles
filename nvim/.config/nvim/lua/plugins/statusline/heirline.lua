17d8614d163f45c5f13faf55a748abd741c33416
lukaaa-m <luka@lukamanitta.com>
2023-01-30 22:06:07 +1000

    feat: move statusline into modular folder

--- nvim/.config/nvim/lua/plugins/statusline/heirline.lua
+++ nvim/.config/nvim/lua/plugins/statusline/heirline.lua
@@ -1,16 +1,5 @@
-local helpers = require("plugins.statusline.helpers")
-local components = require("plugins.statusline.components")
-local get_hi_group_bg = require("utils.color.hl_groups").get_hi_group_bg
+local lines = require("plugins.statusline.lines")
 
 require("heirline").setup({
-    statusline = {
-        components.SingleSpace("Normal"),
-        helpers.bubble_this(components.ModeIndicator, {
-            get_hi_group_bg("Normal"),
-            get_hi_group_bg("StatusLine"),
-        }),
-        components.SingleSpace("StatusLine"),
-        helpers.bubble_this(components.FileName),
-        components.SingleSpace("Normal"),
-    },
+    statusline = lines.BubbleStatusLine,
 })
---
+++ nvim/.config/nvim/lua/plugins/statusline/lines/init.lua
@@ -0,0 +1,3 @@
+return {
+    BubbleStatusLine = require("plugins.statusline.lines.BubbleStatusLine"),
+}
