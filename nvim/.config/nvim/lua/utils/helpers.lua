478e334c2b17f5b37c8eb0a25bc141ba68269c76
lukaaa-m <luka@lukamanitta.com>
2023-01-30 22:07:21 +1000

    feat: add buffer icon helper to get colours

--- nvim/.config/nvim/lua/utils/helpers.lua
+++ nvim/.config/nvim/lua/utils/helpers.lua
@@ -31,11 +31,24 @@ function H.apply_globals(opts)
     end
 end
 
-function H.get_buf_icon(buffer)
-    local filename = vim.api.nvim_buf_get_name(buffer)
+function H.get_buf_icon(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
     local file_ext = string.match(filename, "(%w+)$")
     local default_icon = ""
     return require("nvim-material-icon").get_icon(
+        filename,
+        file_ext,
+        { default = default_icon }
+    )
+end
+
+function H.get_buf_icon_colour(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    local file_ext = string.match(filename, "(%w+)$")
+    local default_icon = ""
+    return require("nvim-material-icon").get_icon_color(
         filename,
         file_ext,
         { default = default_icon }
