8b3f8190adc238d72c6b553a92a9373e0b7dd264
lukaaa-m <luka@lukamanitta.com>
2023-01-31 12:18:03 +1000

    feat(wip): add colourblend for buffer icon heirline component

--- nvim/.config/nvim/lua/plugins/statusline/components/ColouredBufferIcon.lua
+++ nvim/.config/nvim/lua/plugins/statusline/components/ColouredBufferIcon.lua
@@ -1,4 +1,5 @@
 local get_hi_group_bg = require("utils.color.hl_groups").get_hi_group_bg
+-- local blend_rgb = require("utils.color").blend_rgb
 local get_buf_icon_colour = require("utils.helpers").get_buf_icon_colour
 local get_buf_icon = require("utils.helpers").get_buf_icon
 
@@ -9,6 +10,7 @@ return function(bufnr)
         end,
         hl = function()
             local _, colour = get_buf_icon_colour(bufnr)
+            -- colour = blend_rgb(colour, get_hi_group_bg("StatusLine"), 0.5)
             return {
                 fg = colour,
                 -- bg = get_hi_group_bg("StatusLine"),
