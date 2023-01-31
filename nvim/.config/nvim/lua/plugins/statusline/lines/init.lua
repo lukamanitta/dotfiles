975313139a54031c5aead09a9b9bcde497f839c5
lukaaa-m <luka@lukamanitta.com>
2023-01-31 15:52:40 +1000

    feat: add simple center bubble statusline (filename only)

--- nvim/.config/nvim/lua/plugins/statusline/lines/init.lua
+++ nvim/.config/nvim/lua/plugins/statusline/lines/init.lua
@@ -1,4 +1,7 @@
 return {
     BubbleStatusLine = require("plugins.statusline.lines.BubbleStatusLine"),
+    BubbleCenterStatusLine = require(
+        "plugins.statusline.lines.BubbleCenterStatusLine"
+    ),
     VerySimple = require("plugins.statusline.lines.VerySimple"),
 }
