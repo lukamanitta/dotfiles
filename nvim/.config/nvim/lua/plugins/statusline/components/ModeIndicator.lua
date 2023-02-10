82687550fd841207854763732b1166afaf7b217b
lukaaa-m <luka@lukamanitta.com>
2023-02-10 13:09:32 +1000

    feat: remove icon from vim mode indicator

--- nvim/.config/nvim/lua/plugins/statusline/components/ModeIndicator.lua
+++ nvim/.config/nvim/lua/plugins/statusline/components/ModeIndicator.lua
@@ -3,12 +3,11 @@ local get_hi_group_bg = require("utils.color.hl_groups").get_hi_group_bg
 local props = require("plugins.statusline.props")
 
 return tables.table_merge({
-    {
-
-        provider = function()
-            return " " .. require("assets.icons").companies.Vim .. " "
-        end,
-    },
+    -- {
+    --     provider = function()
+    --         return " " .. require("assets.icons").companies.Vim .. " "
+    --     end,
+    -- },
 
     {
         provider = function()
