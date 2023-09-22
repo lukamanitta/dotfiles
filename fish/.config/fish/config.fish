e9970b92b4b5d91ae046f54db7dd3b830fd5d0be
Luka Manitta <luka@lukamanitta.com>
2023-09-22 14:59:32 +1000

    feat: move to rbenv (permission problems with asdf) & set up msi path vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    rbenv init - fish | source
 end
 
-fish_add_path ~/bin/
 
 set -g fish_greeting
 
@@ -10,6 +10,12 @@ set -gx EDITOR kak
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
+fish_add_path ~/bin/
+
 alias lg="lazygit"
+
+# MSI stuff
+source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
+fish_add_path ~/code/msi-utils/bin/
 
 starship init fish | source
