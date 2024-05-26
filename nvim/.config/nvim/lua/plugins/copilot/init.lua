b7a5ed134c9d45f21c800e03b8eccb891e509e70
Luka Manitta <luka@lukamanitta.com>
2024-05-26 19:24:34 +1000

    chore: update nvim plugs

--- nvim/.config/nvim/lua/plugins/copilot/init.lua
+++ nvim/.config/nvim/lua/plugins/copilot/init.lua
@@ -1,7 +1,7 @@
 require("helpers").set_scope_opts(vim.g, {
     copilot_no_tab_map = true,
     copilot_assume_mapped = true,
-    copilot_filetypes = { rust = false },
+    -- copilot_filetypes = { rust = false },
 })
 
 -- require("plugins.plugin").custom_hl
