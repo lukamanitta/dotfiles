b006f0f6bf4c71c2884d4af67c5d51cedc74a26c
Luka Manitta <luka@lukamanitta.com>
2024-10-29 14:12:28 +1000

    feat: add tests to config.fish to reduce errors on new systems

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,21 +6,31 @@ set -g fish_greeting
 
 set -gx EDITOR nvim
 
-export LIBRARY_PATH="$LIBRARY_PATH:/opt/homebrew/lib"
+if type -q brew
+	export LIBRARY_PATH="$LIBRARY_PATH:/opt/homebrew/lib"
+end
+
+# MSI stuff
+if test -d ~/code/msi-utils/bin
+	source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
+	fish_add_path ~/code/msi-utils/bin/
+end
 
 source ~/.asdf/asdf.fish
 # Do this if link isn't already made
 # mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
-fish_add_path ~/bin/
+fish_add_path ~/.local/bin/
 fish_add_path ~/.pub-cache/bin/
 
-source ~/.env
+if test -f ~/.env
+	source ~/.env
+end
 
 alias lg="lazygit"
 
-# MSI stuff
-source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
-fish_add_path ~/code/msi-utils/bin/
+if not type -q starship
+	curl -sS https://starship.rs/install.sh | sh
+end
 
 starship init fish | source
