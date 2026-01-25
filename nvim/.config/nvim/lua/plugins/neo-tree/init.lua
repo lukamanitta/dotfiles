0ae9aa13be4b8e8e7b2ec5e8093a615bee64f588
Luka Manitta <luka@lukamanitta.com>
2026-01-25 22:34:46 +1000

    feat: improve neo-tree icons

--- nvim/.config/nvim/lua/plugins/neo-tree/init.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree/init.lua
@@ -1,4 +1,52 @@
-require("neo-tree").setup({})
+local icons = require("ui.icons")
+
+require("neo-tree").setup({
+    default_component_configs = {
+        icon = {
+            folder_closed = icons.filesystem.Folder,
+            folder_open = icons.filesystem.FolderOpen,
+            folder_empty = icons.filesystem.FolderEmpty,
+            default = icons.filesystem.File,
+            highlight = "NeoTreeFileIcon",
+        },
+        modified = {
+            symbol = icons.general.CircleSmall,
+            highlight = "NeoTreeModified",
+        },
+        git_status = {
+            symbols = {
+                -- Change type
+                added = icons.git.Added,
+                deleted = icons.git.Removed,
+                modified = icons.git.Modified,
+                renamed = icons.git.Renamed,
+                -- Status type
+                untracked = icons.git.Untracked,
+                ignored = icons.git.Ignored,
+                unstaged = icons.git.Unstaged,
+                staged = icons.git.Staged,
+                conflict = icons.git.Merge,
+            },
+        },
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+                "node_modules",
+            },
+            never_show = {
+                ".DS_Store",
+                "thumbs.db",
+            },
+        },
+        follow_current_file = {
+            enabled = true,
+        },
+    },
+})
 
 local background = require("h").get_hl("StatusLine", "bg#")
 
--- nvim/.config/nvim/lua/plugins/nvim-treesitter/init.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter/init.lua
@@ -1,1 +1,44 @@
 -- https://www.reddit.com/r/neovim/comments/1pndf9e/my_new_nvimtreesitter_configuration_for_the_main/
+
+require("nvim-treesitter").setup({})
+
+require("nvim-treesitter")
+    .install({
+        "lua",
+        "rust",
+        "ruby",
+        "python",
+
+        "bash",
+
+        "tsx",
+        "svelte",
+
+        "javascript",
+        "typescript",
+
+        "html",
+        "css",
+        "scss",
+
+        "json",
+        "yaml",
+        "toml",
+
+        "markdown",
+        "markdown_inline",
+    })
+    :wait(300000) -- wait up to 5 minutes for installation to complete
+
+require("nvim-treesitter").update():wait(300000) -- wait up to 5 minutes for update to complete
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        local treesitter = require("nvim-treesitter")
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if vim.list_contains(treesitter.get_installed(), lang) then
+            vim.treesitter.start(args.buf)
+        end
+    end,
+    desc = "Enable nvim-treesitter for supported filetypes",
+})
