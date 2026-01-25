e162a3cb4b400dc305d1facfe15fcc580a06e0e8
Luka Manitta <luka@lukamanitta.com>
2026-01-25 15:29:18 +1000

    feat: add vi mode keybinds to fish

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,11 @@
 function fish_user_key_bindings
+    fish_vi_key_bindings
+
+    bind -M insert -m default jk repaint
+
+    bind -M insert \cf accept-autosuggestion
+    bind \cf accept-autosuggestion
+
     bind ! bind_bang
     bind '$' bind_dollar
 end
