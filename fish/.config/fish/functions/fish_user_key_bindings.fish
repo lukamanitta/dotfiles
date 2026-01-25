cd5fd92668ab9af1f0fe819b8a3d74275767ecef
Luka Manitta <luka@lukamanitta.com>
2026-01-25 22:34:39 +1000

    feat: fix bash !! style binds for fish vi mode

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -6,6 +6,6 @@ function fish_user_key_bindings
     bind -M insert \cf accept-autosuggestion
     bind \cf accept-autosuggestion
 
-    bind ! bind_bang
-    bind '$' bind_dollar
+    bind -M insert ! bind_bang
+    bind -M insert '$' bind_dollar
 end
