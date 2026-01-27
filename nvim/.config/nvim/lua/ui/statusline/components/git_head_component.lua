daa2112660c7c271de98ed2794d96f4350116283
Luka Manitta <luka@lukamanitta.com>
2026-01-27 13:36:20 +1000

    feat: adjust autocmd triggers for statusline git head component

--- nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
+++ nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
@@ -1,7 +1,12 @@
 local git_head_name = ""
 
-vim.api.nvim_create_autocmd({ "DirChanged", "BufEnter", "BufWinEnter" }, {
+-- "VimEnter" not working...
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufReadPost" }, {
+    pattern = "*",
     callback = function()
+        if vim.bo.filetype == "TelescopePrompt" or vim.bo.filetype == "neo-tree" then
+            return
+        end
         local head = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
         if not head or head == "" or head:find("fatal") then
             git_head_name = ""
