849a9d26c29b35076e2804366fe4b264ee784d16
Luka Manitta <luka@lukamanitta.com>
2026-01-27 13:28:05 +1000

    feat: fix extreme slowness on macos caused by re-evaluating git head on every statusline render - now evaluates only when changing directory or buffer

--- nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
+++ nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
@@ -1,8 +1,17 @@
-return function()
-    local head = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-    if not head or head == "" or head:find("fatal") then
-        return ""
-    end
+local git_head_name = ""
 
-    return string.format(require("ui.icons").git.Branch .. " %s", head)
+vim.api.nvim_create_autocmd({ "DirChanged", "BufEnter", "BufWinEnter" }, {
+    callback = function()
+        local head = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+        if not head or head == "" or head:find("fatal") then
+            git_head_name = ""
+        else
+            git_head_name = head
+        end
+    end,
+})
+
+
+return function()
+    return string.format(require("ui.icons").git.Branch .. " %s", git_head_name)
 end
