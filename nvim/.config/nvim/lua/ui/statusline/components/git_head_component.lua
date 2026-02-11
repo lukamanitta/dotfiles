ef49463eaa84e2febb93f6a2cda0f07f3cddcd7e
Luka Manitta <luka@lukamanitta.com>
2026-02-11 11:41:00 +1000

    fix: don't display git branch icon if not in git repo

--- nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
+++ nvim/.config/nvim/lua/ui/statusline/components/git_head_component.lua
@@ -4,19 +4,22 @@ local git_head_name = ""
 vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufReadPost" }, {
     pattern = "*",
     callback = function()
-        if vim.bo.filetype == "TelescopePrompt" or vim.bo.filetype == "neo-tree" then
+        if
+            vim.bo.filetype == "TelescopePrompt"
+            or vim.bo.filetype == "neo-tree"
+        then
             return
         end
         local head = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
         if not head or head == "" or head:find("fatal") then
             git_head_name = ""
         else
-            git_head_name = head
+            git_head_name =
+                string.format(require("ui.icons").git.Branch .. " %s", head)
         end
     end,
 })
 
-
 return function()
-    return string.format(require("ui.icons").git.Branch .. " %s", git_head_name)
+    return git_head_name
 end
